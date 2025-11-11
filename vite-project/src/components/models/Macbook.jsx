90d5ea7a4c2ba01d23a8dc01ea0faafbe52d4b44
adair <adair@debian.adair.com>
2025-11-11 00:28:21 -0800

    implement features

--- vite-project/src/components/models/Macbook.jsx
+++ vite-project/src/components/models/Macbook.jsx
@@ -1,9 +1,29 @@
 
-import React from 'react'
-import { useGLTF, useTexture } from '@react-three/drei'
+import React, { useEffect } from 'react'
+import { useGLTF, useTexture, useVideoTexture } from '@react-three/drei'
+import useMacbookStore from '../../store'
+import { Color, SRGBColorSpace } from 'three'
+import { noChangeParts } from '../../constants'
 
 export default function MacbookModel(props) {
-  const { nodes, materials } = useGLTF('/models/macbook-transformed.glb')
+    const { color, texture } = useMacbookStore();
+  const { nodes, materials, scene } = useGLTF('/models/macbook-transformed.glb')
+
+    const screen = useVideoTexture(texture)
+
+  screen.colorSpace = SRGBColorSpace;
+  screen.needsUpdate = true;
+
+  useEffect(() => {
+    scene.traverse((child) =>{
+      if(child.isMesh) {
+        if(!noChangeParts.includes(child.name)){
+          child.material.color = new Color(color);
+        }
+      }
+    })
+  }, [color, scene])
+
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.Object_10.geometry} material={materials.PaletteMaterial001} rotation={[Math.PI / 2, 0, 0]} />
@@ -23,7 +43,9 @@ export default function MacbookModel(props) {
       <mesh geometry={nodes.Object_82.geometry} material={materials.gMtYExgrEUqPfln} rotation={[Math.PI / 2, 0, 0]} />
       <mesh geometry={nodes.Object_96.geometry} material={materials.PaletteMaterial003} rotation={[Math.PI / 2, 0, 0]} />
       <mesh geometry={nodes.Object_107.geometry} material={materials.JvMFZolVCdpPqjj} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.Object_123.geometry} material={materials.sfCQkHOWyrsLmor} rotation={[Math.PI / 2, 0, 0]} />
+      <mesh geometry={nodes.Object_123.geometry} rotation={[Math.PI / 2, 0, 0]}>
+        <meshBasicMaterial map={screen} />
+      </mesh>
       <mesh geometry={nodes.Object_127.geometry} material={materials.ZCDwChwkbBfITSW} rotation={[Math.PI / 2, 0, 0]} />
     </group>
   )
